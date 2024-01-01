8c3fd7c83af65df9f36fe1eb566feaa001110b7b
rnet <chinaitlearner@gmail.com>
2024-01-02 04:49:13 +0800

    fix: code logging optimization，upload to npm

--- main.js
+++ main.js
@@ -9,14 +9,63 @@ const { logger, paths, isValidUrl } = utils;
 const pkg = require(paths.package);
 const request = require('request-promise');
 const cheerio = require('cheerio');
+const log4js = require('log4js');
 
-function debugLog() {
-  if (pkg.logLevel === 'debug') {
-    logger.log('execPath:', __dirname);
-    logger.log('filePath:', __filename);
-    logger.log('processCwd:', process.cwd());
-    logger.log('paths:\n', JSON.stringify(paths, null, 2));
+function debugLog(level) {
+  if (level) {
+    if (!log4js.levels.levels.map(it => it.levelStr).includes(level.toUpperCase())) {
+      throw new Error('日志等级输入错误，请检查!');
+    }
+    logger.level = level;
   }
+  logger.trace('execPath:', __dirname);
+  logger.trace('filePath:', __filename);
+  logger.trace('processCwd:', process.cwd());
+  logger.trace('paths:\n', JSON.stringify(paths, null, 2));
+}
+
+const commandBuilder = {
+  f: {
+    alias: 'file',
+    describe: '含有nsd, cd值的json文件',
+    type: 'string',
+    coerce: (input) => {
+      if (['1', '2'].includes(input)) input = paths.exampleResolve(`codes/${input}-\$_ts.json`);
+      if (!fs.existsSync(input)) throw new Error(`输入文件不存在: ${input}`);
+      return JSON.parse(fs.readFileSync(paths.resolve(input), 'utf8'));
+    }
+  },
+  l: {
+    alias: 'level',
+    describe: '日志打印等级，参考log4js，默认为info',
+    type: 'string',
+  },
+  u: {
+    alias: 'url',
+    describe: '瑞数返回204状态码的请求地址',
+    type: 'string',
+    coerce: async (input) => {
+      if (!isValidUrl(input)) throw new Error('输入链接不正确');
+      try {
+        const res = await request(input)
+        const $ = cheerio.load(res);
+        const scripts = [...$('script')].map(ele => $(ele).text())
+        .filter(text => text.includes('$_ts.nsd') && text.includes('$_ts.cd'));
+        if (!scripts.length) throw new Error('链接返回结果未找到cd或nsd');
+        return Function('window', scripts[0] + 'return $_ts')({});
+      } catch(err) {
+        throw new Error('输入链接无效');
+      }
+    }
+  }
+}
+
+const commandHandler = (command, argv) => {
+  debugLog(argv.level);
+  const ts = argv.file || argv.url || require(paths.exampleResolve('codes/1-\$_ts.json'));
+  logger.trace(`传入的$_ts.nsd: ${ts.nsd}`);
+  logger.trace(`传入的$_ts.cd: ${ts.cd}`);
+  command(ts);
 }
 
 module.exports = yargs
@@ -27,76 +76,14 @@ module.exports = yargs
   .command({
     command: 'makecode',
     describe: '生成动态代码',
-    builder: {
-      f: {
-        alias: 'file',
-        describe: '含有nsd, cd值的json文件',
-        type: 'string',
-        coerce: (input) => {
-          if (!fs.existsSync(input)) throw new Error('输入文件不存在');
-          return JSON.parse(fs.readFileSync(paths.resolve(input), 'utf8'));
-        }
-      },
-      u: {
-        alias: 'url',
-        describe: '瑞数返回204状态码的请求地址',
-        type: 'string',
-        coerce: async (input) => {
-          if (!isValidUrl(input)) throw new Error('输入链接不正确');
-          try {
-            const res = await request(input)
-            const $ = cheerio.load(res);
-            const scripts = [...$('script')].map(ele => $(ele).text())
-              .filter(text => text.includes('$_ts.nsd') && text.includes('$_ts.cd'));
-            if (!scripts.length) throw new Error('链接返回结果未找到cd或nsd');
-            return Function('window', scripts[0] + 'return $_ts')({});
-          } catch(err) {
-            throw new Error('输入链接无效');
-          }
-        }
-      }
-    },
-    handler: (argv) => {
-      debugLog();
-      makeCode(argv.file || argv.url);
-    },
+    builder: commandBuilder,
+    handler: commandHandler.bind(null, makeCode),
   })
   .command({
     command: 'makecookie',
     describe: '生成动态cookie',
-    builder: {
-      f: {
-        alias: 'file',
-        describe: '含有nsd, cd值的json文件',
-        type: 'string',
-        coerce: (input) => {
-          if (!fs.existsSync(input)) throw new Error('输入文件不存在');
-          return JSON.parse(fs.readFileSync(paths.resolve(input), 'utf8'));
-        }
-      },
-      u: {
-        alias: 'url',
-        describe: '瑞数返回204状态码的请求地址',
-        type: 'string',
-        coerce: async (input) => {
-          if (!isValidUrl(input)) throw new Error('输入链接不正确');
-          try {
-            const res = await request(input)
-            const $ = cheerio.load(res);
-            const scripts = [...$('script')].map(ele => $(ele).text())
-              .filter(text => text.includes('$_ts.nsd') && text.includes('$_ts.cd'));
-            if (!scripts.length) throw new Error('链接返回结果未找到cd或nsd');
-            return Function('window', scripts[0] + 'return $_ts')({});
-          } catch(err) {
-            throw new Error('输入链接无效');
-          }
-        }
-      }
-    },
-    handler: (argv) => {
-      debugLog();
-      makeCookie(argv.file || argv.url);
-    },
+    builder: commandBuilder,
+    handler: commandHandler.bind(null, makeCookie),
   })
   .updateStrings({
     'Show version number': '显示版本号',
@@ -104,6 +91,8 @@ module.exports = yargs
   })
   .example('$0 makecode -f example/codes/1-\$_ts.json')
   .example('$0 makecode -u http://url/path')
+  .example('$0 makecookie -f example/codes/1-\$_ts.json')
+  .example('$0 makecookie -u http://url/path')
   .epilog('')
   .argv;
 
--- script/runcode.js
+++ script/runcode.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
+// 可以快速运行rs-reverse有关的js代码，用于开发或者演示时使用
 require('module-alias/register');
 Math.random = () => 0.1253744220839037;
 const gv = require('../utils/initGv');
 Object.assign(global, gv.utils);
-process.argv.slice(2).forEach(text => console.log(JSON.stringify(eval(text))));
+process.argv.slice(2).forEach(input => {
+  const output = JSON.stringify(eval(input));
+  console.log([`\n  输入：${input}`, `输出：${output}\n`].join('\n  '));
+});
